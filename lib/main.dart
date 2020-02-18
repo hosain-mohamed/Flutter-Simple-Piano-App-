9e98c4ac8529e20f41ab1a05d698a49dba9dc371
shs442000 <shs442000@gmail.com>
2020-02-18 16:40:13 +0200

    First commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:assets_audio_player/assets_audio_player.dart';
 
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  void playSong(int songNumber) {
+    final audio1 = AssetsAudioPlayer();
+    audio1.open('assets/note$songNumber.wav');
+    audio1.play();
+  }
+
+  Widget buildPianoButton(int songNumber, int colorDegree) {
+    return Expanded(
+      child: FlatButton(
+        color: Colors.blue[colorDegree],
+        onPressed: () {
+          playSong(songNumber);
+        },
+      ),
+    );
+  }
+
+  List<Widget> getListOfPianoButtons() {
+    List<Widget> list = new List<Widget>();
+    for (int i = 1; i < 8; i++) {
+      list.add(buildPianoButton(i, i * 100));
+    }
+    return list ;
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: getListOfPianoButtons(),
+              ),
         ),
       ),
     );
